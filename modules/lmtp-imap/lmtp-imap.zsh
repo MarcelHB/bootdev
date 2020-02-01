0a41ea2ffc35fb7b2de008d1cb5acee1adb80982
MarcelHB <7heing@informatik.uni-hamburg.de>
2020-02-02 00:29:18 +0100

    scripts: fixes some shellcheck warnings

--- modules/lmtp-imap/lmtp-imap.zsh
+++ modules/lmtp-imap/lmtp-imap.zsh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/sh
 
 new_imap_user () {
   if [ "$#" -ne 2 ]; then
@@ -9,7 +9,7 @@ new_imap_user () {
 
   echo "~> Getting netmask for $NETWORK_ADAPTER."
 
-  NET_CIDR=`ipcalc $(ip -s -o address show dev $NETWORK_ADAPTER | grep '"'"'inet '"'"' | awk '"'"'{ print $4 }'"'"') | grep Network | awk '"'"'{ print $2 }'"'"'`
-  PASSWORD=$(doveadm pw -p $2)
+  NET_CIDR=$(ipcalc "$(ip -s -o address show dev $NETWORK_ADAPTER | grep '"'"'inet '"'"' | awk '"'"'{ print $4 }'"'"')" | grep Network | awk '"'"'{ print $2 }'"'"')
+  PASSWORD=$(doveadm pw -p "$2")
   echo "$1:$PASSWORD::::::allow_nets=$NET_CIDR" | sudo tee /etc/dovecot/users > /dev/null
 }
