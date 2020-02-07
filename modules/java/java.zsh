0280c67cef97693437653a6e84efc5e8f768b4a3
MarcelHB <7heing@informatik.uni-hamburg.de>
2020-02-07 19:06:04 +0100

    java: adds shortcut for mvn dep. security audit

--- modules/java/java.zsh
+++ modules/java/java.zsh
@@ -1,2 +1,5 @@
 #!/bin/sh
+# extracts the project version from the pom.xml
 alias mvn-version='mvn org.apache.maven.plugins:maven-help-plugin:3.1.1:evaluate -Dexpression=project.version | grep -v \"^\[\"'
+# creates an HTML report of dep. vulnerabilities in target/
+alias mvn-vuln='mvn org.owasp:dependency-check-maven:5.3.0:check'
