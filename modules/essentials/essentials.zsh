f1f2ed8aee159c316f2ec68cf3fd12ffc1a046c1
MarcelHB <7heing@informatik.uni-hamburg.de>
2020-04-05 12:39:24 +0200

    essentials: adds substring history search

--- modules/essentials/essentials.zsh
+++ modules/essentials/essentials.zsh
@@ -14,3 +14,7 @@ alias ide='tmux new-session \"nvim\"'
 # xclip
 alias xc='xclip -selection clipboard'
 alias xp='xclip -o -selection clipboard'
+
+# type term, then arrow up/down
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
