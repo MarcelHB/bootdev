ed574ad96730ef109ef94607d3bdbf8e644d2a83
MarcelHB <7heing@informatik.uni-hamburg.de>
2020-04-05 13:21:06 +0200

    java: version bump of alias packages

--- modules/java/java.zsh
+++ modules/java/java.zsh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # extracts the project version from the pom.xml
-alias mvn-version='mvn org.apache.maven.plugins:maven-help-plugin:3.1.1:evaluate -Dexpression=project.version | grep -v \"^\[\"'
+alias mvn-version='mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version | grep -v \"^\[\"'
 # creates an HTML report of dep. vulnerabilities in target/
-alias mvn-vuln='mvn org.owasp:dependency-check-maven:5.3.0:check'
+alias mvn-vuln='mvn org.owasp:dependency-check-maven:5.3.2:check'
