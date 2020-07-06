6baaa8bea6cb53e5f056fbce306225bcb1603e23
MarcelHB <7heing@informatik.uni-hamburg.de>
2020-07-06 18:37:40 +0200

    essentials: drop of custom git shortcuts

--- modules/essentials/essentials.zsh
+++ modules/essentials/essentials.zsh
@@ -2,13 +2,6 @@
 # ag
 alias ag='ag --path-to-ignore ~/.ignore'
 
-# git
-alias gits='git status'
-alias gitc='git commit'
-alias gitp='git push'
-alias gitpf='git push --force-with-lease'
-alias gitpr='git pull --rebase'
-
 alias ide='tmux new-session \"nvim\"'
 
 # xclip
