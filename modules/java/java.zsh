6c3e1f50429fe3af1d37ec13f9c074d2d81bc0e0
MarcelHB <marscel@googlemail.com>
2022-03-25 15:02:40 +0100

    java: update of CVE scanner version

--- modules/java/java.zsh
+++ modules/java/java.zsh
@@ -2,7 +2,7 @@
 # extracts the project version from the pom.xml
 alias mvn-version='mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version | grep -v \"^\[\"'
 # creates an HTML report of dep. vulnerabilities in target/
-alias mvn-vuln='mvn org.owasp:dependency-check-maven:5.3.2:check'
+alias mvn-vuln='mvn org.owasp:dependency-check-maven:7.0.1:check'
 
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
