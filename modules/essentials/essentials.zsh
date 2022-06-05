849ae3f0d236af81e0599c419f8fedfd8d13a89b
MarcelHB <marscel@googlemail.com>
2022-06-05 14:50:00 +0200

    essentials: adds shortcut to list dpkg bloat

--- modules/essentials/essentials.zsh
+++ modules/essentials/essentials.zsh
@@ -11,3 +11,5 @@ alias xp='xclip -o -selection clipboard'
 # type term, then arrow up/down
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
+
+alias dpkg-bloat='dpkg-query -Wf '"'"'${db:Status-Status} ${Installed-Size}\t${Package}\n'"'"' | sed -ne '"'"'s/^installed //p'"'"' | sort -n'
